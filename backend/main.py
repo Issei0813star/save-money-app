408b29d45e9f6b30f60540afbe0801875b9164d8
isseistar <issei0813star@gmail.com>
2023-06-05 00:09:15 +0900

    環境構築

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+from api import api_bp
 
-app = Flask(__name__, static_folder='../frontend/dist/static', template_folder='../frontend/dist')
+app = Flask(
+    __name__,
+    static_folder="../frontend/dist/static",
+    template_folder="../frontend/dist",
+)
+app.register_blueprint(api_bp)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
 def index(path):
-    return render_template('index.html')
+    return render_template("index.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
