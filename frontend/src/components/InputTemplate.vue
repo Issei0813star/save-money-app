bae340fb34ce47fbc5647ce8402d427255b36c4c
isseistar <issei0813star@gmail.com>
2023-06-05 02:53:01 +0900

    入力画面の雛形作成

---
+++ frontend/src/components/InputTemplate.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="input-template">
+    <div class="input-field">
+      <div class="text-field">
+        <v-text-field
+          label="日付"
+          placeholder="日付を入力"
+          color="blue"
+          clearable
+        >
+        </v-text-field>
+        <v-text-field
+          label="金額"
+          placeholder="金額を入力"
+          color="blue"
+          clearable
+        >
+        </v-text-field>
+      </div>
+      <div class="category-field">
+        <p>カテゴリー</p>
+      </div>
+    </div>
+    <v-btn variant="outlined"> {{ paymentTypeData }}を登録 </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    paymentType: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    paymentTypeData() {
+      return this.paymentType === 'spending' ? '支出' : '収入'
+    },
+  },
+}
+</script>
+
+<style>
+.input-template {
+  margin-top: 50px;
+}
+
+.input-field {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+}
+
+.text-field {
+  width: 300px;
+}
+
+.category-field {
+  width: 300px;
+}
+</style>
--- frontend/src/views/InputPayments.vue
+++ frontend/src/views/InputPayments.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
-    <h1>入力</h1>
+    <nav>
+      <v-btn @click="paymentType = 'spending'">支出</v-btn>
+      <v-btn @click="paymentType = 'income'">収入</v-btn>
+    </nav>
+    <input-template :payment-type="paymentType"></input-template>
   </div>
 </template>
 
 <script>
+import InputTemplate from '../components/InputTemplate.vue'
 export default {
   name: 'InputPayments',
+  components: {
+    InputTemplate,
+  },
+  data: function () {
+    return {
+      paymentType: 'spending',
+    }
+  },
 }
 </script>
