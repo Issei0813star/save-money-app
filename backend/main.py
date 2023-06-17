6e0d7651dd617ea8e2f6eeb5ccea962128bc38d8
isseistar <issei0813star@gmail.com>
2023-06-18 01:04:21 +0900

    収支登録用のAPI作成

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,47 @@
-from flask import Flask, render_template
-from api import api_bp
+from flask import Flask, render_template, request
+from flask_restful import Api, Resource, reqparse
+import sqlite3
 
 app = Flask(
     __name__,
     static_folder="../frontend/dist/static",
     template_folder="../frontend/dist",
 )
-app.register_blueprint(api_bp)
+api = Api(app)
+# リクエストパーサーを作成
+parser = reqparse.RequestParser()
+parser.add_argument("amount", type=int, required=True)
+parser.add_argument("type", type=str, required=True)
+parser.add_argument("category", type=str, required=True)
+parser.add_argument("isCredit", type=int, required=True)
+parser.add_argument("date", type=str, required=True)
+
+
+class Payment(Resource):
+    def post(self):
+        args = parser.parse_args()
+
+        conn = sqlite3.connect("../save-money-app.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "INSERT INTO payment (amount, type, category, isCredit, date) VALUES (?, ?, ?, ?, ?)",
+            (
+                args["amount"],
+                args["type"],
+                args["category"],
+                args["isCredit"],
+                args["date"],
+            ),
+        )
+
+        conn.commit()
+
+        conn.close()
+        return {"message": "データが正常に保存されました。"}, 200
+
+
+api.add_resource(Payment, "/api/payment")
 
 
 @app.route("/", defaults={"path": ""})
